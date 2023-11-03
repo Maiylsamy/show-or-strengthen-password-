3176b366d3b064c3862ea6e40c1f735da996d484
Maiylsamy <maiylsamy310@gmail.com>
2023-11-03 16:10:22 +0530

    strong password

--- pword.js
+++ pword.js
@@ -1,5 +1,10 @@
 let eye = document.getElementById("eye");
 let password = document.getElementById("password");
+let msg=document.getElementById("message");
+let str=document.getElementById("strength");
+
+
+
 eye.addEventListener("click", () => {
   if (password.type == "password") {
     password.type = "text";
@@ -14,5 +19,32 @@ eye.addEventListener("click", () => {
   }
 
 });
+
+password.addEventListener('input',()=>{
+    if(password.value.length>0){
+        msg.style.display="block";
+
+    }else{
+        msg.style.display="none";
+    };
+
+    if(password.value.length < 4){
+        str.innerHTML="weak!";
+        msg.style.color="red";
+
+    }else if(password.value.length >= 4 && password.value.length <8 ){
+        str.innerHTML = "medium.";
+        msg.style.color="blue";
+
+
+        
+    }else if(password.value.length>8){
+        str.innerHTML ="strong.";
+        msg.style.color="green";
+
+    }
+
+
+})
 
     
